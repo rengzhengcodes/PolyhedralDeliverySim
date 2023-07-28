185736a384ac8430294932071539c7ac2bdb57e1
Renggeng Zheng <rzhengcontacts@gmail.com>
2023-07-28 23:02:24 +0000

    did the first two compositions correctly, i think

--- main.cpp
+++ main.cpp
@@ -35,5 +35,23 @@ int main(int argc, char* argv[])
  */
 std::string analyze_latency(isl_map *p_src_occupancy, isl_map *p_dst_fill)
 {
-    return std::string("NOT IMPLEMENTED YET!!!");
+    // Wraps dst fill such that the binary relation implies data.
+    isl_map *dst_fill_wrapped = isl_map_range_map(p_dst_fill);
+    // Prints out dst_fill_wrapped in the terminal.
+    isl_map_dump(dst_fill_wrapped);
+    
+    // Inverts src_occupancy such that data implies source.
+    isl_map *src_occupancy_inverted = isl_map_reverse(p_src_occupancy);
+    // Prints out src_occupancy_inverted in the terminal.
+    isl_map_dump(src_occupancy_inverted);
+
+    // Composites dst_fill_wrapped and src_occupancy_inverted such that data
+    // implies source.
+    isl_map *dst_fill_wrapped_composed = isl_map_apply_range(
+        dst_fill_wrapped,
+        src_occupancy_inverted
+    );
+    
+
+    return "Coding In Progress...";
 }
