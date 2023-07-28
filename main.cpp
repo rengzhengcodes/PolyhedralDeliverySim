38ea56a17f2d100260f4c71031c9d8d869d36324
Renggeng Zheng <rzhengcontacts@gmail.com>
2023-07-28 21:45:43 +0000

    cleaned up some comments

--- main.cpp
+++ main.cpp
@@ -4,20 +4,16 @@ int main(int argc, char* argv[])
 {
     // Allocates space to the isl context and returns a pointer to it.
     isl_ctx *p_ctx = isl_ctx_alloc();
-    /**
-     * Reads a map from a string relating source location and the data occupied
+    /* Reads a map from a string relating source location and the data occupied
      * within. The map is represented using a binary relational diagram where
-     * source implies data.
-     */
+     * source implies data.*/
     isl_map *p_src_occupancy = isl_map_read_from_str(
         p_ctx,
         "{ [xs, ys] -> [d0, d1] : d0=xs and 0 <= d1 < 8 and 0 <= xs < 8 and 0 <= ys < 8 }"
     );
-    /**
-     * Reads a map froma  string relating destination location and the data
+    /* Reads a map from a string relating destination location and the data
      * requested. The map is represented using a binary relational diagram where
-     * source implies data.
-     */
+     * source implies data. */
     isl_map *p_dst_fill = isl_map_read_from_str(
         p_ctx,
         "{ [xd, yd] -> [d0, d1] : d0=xd and d1=yd and 0 <= xd < 8 and 0 <= yd < 8 }"
@@ -27,6 +23,9 @@ int main(int argc, char* argv[])
     std::cout << result << std::endl;
 }
 
+/**
+ * 
+ */
 std::string analyze_latency(isl_map *p_src_occupancy, isl_map *p_dst_fill)
 {
     return std::string("NOT IMPLEMENTED YET!!!");
