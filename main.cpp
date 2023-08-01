16eb81f17842a70e0b9e1f9feb40a3133a5d3a9e
Renggeng Zheng <rzhengcontacts@gmail.com>
2023-08-01 23:03:05 +0000

    made intermediate dumps a function

--- main.cpp
+++ main.cpp
@@ -9,14 +9,14 @@ int main(int argc, char* argv[])
      * source implies data. */
     isl_map *p_src_occupancy = isl_map_read_from_str(
         p_ctx,
-        "{ [xs, ys] -> [d0, d1] : d0=xs and 0 <= d1 < 8 and 0 <= xs < 8 and 0 <= ys < 8 }"
+        "{ [xs, ys] -> [d0, d1] : d0=xs and d1=ys and 0 <= xs < 8 and 0 <= ys < 8 }"
     );
     /* Reads a map from a string relating destination location and the data
      * requested. The map is represented using a binary relational diagram where
      * source implies data. */
     isl_map *p_dst_fill = isl_map_read_from_str(
         p_ctx,
-        "{ [xd, yd] -> [d0, d1] : d0=xd and d1=yd and 0 <= xd < 8 and 0 <= yd < 8 }"
+        "{ [xd, yd] -> [d0, d1] : d0=xd and 0 <= d1 < 8 and 0 <= xd < 8 and 0 <= yd < 8 }"
     );
     /* Defines an ISL distance function that calculates the manhattan distance
      * between two points. */
@@ -33,15 +33,13 @@ int main(int argc, char* argv[])
                 "xd >= xs and yd < ys"
         "}"
     );
-    std::cout << "manhattan_metric: " << std::endl;
-    isl_pw_aff_dump(manhattan_metric);
+    dump("manhattan_metric: ", manhattan_metric);
     
     // Turns the above piecewise affine into a map.
     isl_map *manhattan_metric_map = isl_map_from_pw_aff(
         isl_pw_aff_copy(manhattan_metric)
     );
-    std::cout << "manhattan_metric_map: " << std::endl;
-    isl_map_dump(manhattan_metric_map);
+    dump("manhattan_metric_map: ", manhattan_metric_map);
 
   
     std::string result = analyze_latency(p_src_occupancy, p_dst_fill, manhattan_metric_map);
@@ -62,27 +60,22 @@ int main(int argc, char* argv[])
 std::string analyze_latency(isl_map *p_src_occupancy, isl_map *p_dst_fill, isl_map *dist_func)
 {
     // Prints out the inputs.
-    std::cout << "\np_src_occupancy: " << std::endl;
-    isl_map_dump(p_src_occupancy);
-    std::cout << "p_dst_fill: " << std::endl;
-    isl_map_dump(p_dst_fill);
-    std::cout << "dist_func: " << std::endl;
-    isl_map_dump(dist_func);
+    dump("\np_src_occupancy: ", p_src_occupancy);
+    dump("p_dst_fill: ", p_dst_fill);
+    dump("dist_func: ", dist_func);
 
     /* Invertsdst_fill such that data implies dst.
      * i.e. {[xd, yd] -> [d0, d1]} becomes {[d0, d1] -> [xs, ys]} */
     isl_map *dst_fill_inverted = isl_map_reverse(
         isl_map_copy(p_dst_fill)
     );
-    std::cout << "\ndst_fill_inverted: " << std::endl;
-    isl_map_dump(dst_fill_inverted);
+    dump("\ndst_fill_inverted: ", dst_fill_inverted);
     /* Inverts src_occupancy such that data implies source.
      * i.e. {[xs, ys] -> [d0, d1]} becomes {[d0, d1] -> [xs, ys]} */
     isl_map *src_occupancy_inverted = isl_map_reverse(
         isl_map_copy(p_src_occupancy)
     );
-    std::cout << "src_occupancy_inverted: " << std::endl;
-    isl_map_dump(src_occupancy_inverted);
+    dump("src_occupancy_inverted: ", src_occupancy_inverted);
 
     /* Takes the factored range of src_occupancy_inverted and dst_fill_inverted
      * to get {[d0, d1] -> [[xd, yd] -> [xs, ys]]} */
@@ -90,15 +83,13 @@ std::string analyze_latency(isl_map *p_src_occupancy, isl_map *p_dst_fill, isl_m
             isl_map_copy(dst_fill_inverted),
             isl_map_copy(src_occupancy_inverted)
     );
-    std::cout << "\ndata_TO_dst_to_src: " << std::endl;
-    isl_map_dump(data_TO_dst_to_src);
+    dump("\ndata_TO_dst_to_src: ", data_TO_dst_to_src);
     /* Wraps dst fill such that the binary relation implies data.
      * i.e. {[[xd, yd] -> [d0, d1]] -> [d0, d1]} */
     isl_map *dst_fill_wrapped = isl_map_range_map(
         isl_map_copy(p_dst_fill)
     );
-    std::cout << "dst_fill_wrapped: " << std::endl;
-    isl_map_dump(dst_fill_wrapped);
+    dump("dst_fill_wrapped: ", dst_fill_wrapped);
 
     /* Composites dst_fill_wrapped and data_to_dst_to_src to get
      * {[[xd, yd] -> [d0, d1]] -> [[xd', yd'] -> [xs, ys]]} */
@@ -106,12 +97,11 @@ std::string analyze_latency(isl_map *p_src_occupancy, isl_map *p_dst_fill, isl_m
         isl_map_copy(dst_fill_wrapped),
         isl_map_copy(data_TO_dst_to_src)
     );
-    std::cout << "\ndst_to_data_TO_dst_to_src: " << std::endl;
-    isl_map_dump(dst_to_data_TO_dst_to_src);
+    dump("\ndst_to_data_TO_dst_to_src: ", dst_to_data_TO_dst_to_src);
 
-
+    if (islIntermediates) std::cout << '\n' << std::endl;
     // Restricts the range such that xd' = xd and yd' = yd.
-    for (int i = 0; i < isl_map_dim(dst_to_data_TO_dst_to_src, isl_dim_in); i++)
+    for (int i = 0; i < isl_map_dim(p_dst_fill, isl_dim_in); i++)
     {
         dst_to_data_TO_dst_to_src = isl_map_equate(
             dst_to_data_TO_dst_to_src,
@@ -120,9 +110,10 @@ std::string analyze_latency(isl_map *p_src_occupancy, isl_map *p_dst_fill, isl_m
             isl_dim_out,
             i
         );
+        dump("dst_to_data_TO_dst_to_src_restricting_"+std::to_string(i)+": ", 
+             dst_to_data_TO_dst_to_src);
     };
-    std::cout << "dst_to_data_TO_dst_to_src_restricted: " << std::endl;
-    isl_map_dump(dst_to_data_TO_dst_to_src);
+    dump("\ndst_to_data_TO_dst_to_src_restricted: ", dst_to_data_TO_dst_to_src);
 
     /* Computes the manhattan distance between the destination for a data and
      * a source for that data. */
@@ -130,22 +121,19 @@ std::string analyze_latency(isl_map *p_src_occupancy, isl_map *p_dst_fill, isl_m
         isl_map_copy(dst_to_data_TO_dst_to_src),
         isl_map_copy(dist_func)
     );
-    std::cout << "manhattan_distance: " << std::endl;
-    isl_map_dump(manhattan_distance);
+    dump("\nmanhattan_distance: ", manhattan_distance);
 
     // Computes the minimum distance from every source to every destination.
     isl_multi_pw_aff *min_distance = isl_map_min_multi_pw_aff(
         isl_map_copy(manhattan_distance)
     );
-    std::cout << "\nmin_distance: " << std::endl;
-    isl_multi_pw_aff_dump(min_distance);
+    dump("\nmin_distance: ", min_distance);
 
     // Computes the maximum of minimum distances for every data.
     isl_multi_val *max_min_distance = isl_multi_pw_aff_max_multi_val(
         isl_multi_pw_aff_copy(min_distance)
     );
-    std::cout << "\nmax_min_distance: " << std::endl;
-    isl_multi_val_dump(max_min_distance);
+    dump("\nmax_min_distance: ", max_min_distance);
 
     return "Coding In Progress...";
 }
--- main.hpp
+++ main.hpp
@@ -8,3 +8,45 @@
 #include <isl/map.h>
 
 std::string analyze_latency(isl_map *p_src_occupancy, isl_map *p_dst_fill, isl_map *dist_func);
+
+// Defines debug variables from environment variables.
+#include <string.h>
+bool islIntermediates = (getenv("ISL_INTERMEDIATES") != NULL) &&
+                        (strcmp(getenv("ISL_INTERMEDIATES"), "0") != 0);
+
+// Defines debug dump function.
+void dump(std::string str, isl_map *map)
+{
+    if (islIntermediates)
+    {
+        std::cout << str << std::endl;
+        isl_map_dump(map);
+    }
+}
+
+void dump(std::string str, isl_pw_aff *pw_aff)
+{
+    if (islIntermediates)
+    {
+        std::cout << str << std::endl;
+        isl_pw_aff_dump(pw_aff);
+    }
+}
+
+void dump(std::string str, isl_multi_pw_aff *multi_pw_aff)
+{
+    if (islIntermediates)
+    {
+        std::cout << str << std::endl;
+        isl_multi_pw_aff_dump(multi_pw_aff);
+    }
+}
+
+void dump(std::string str, isl_multi_val *multi_val)
+{
+    if (islIntermediates)
+    {
+        std::cout << str << std::endl;
+        isl_multi_val_dump(multi_val);
+    }
+}
