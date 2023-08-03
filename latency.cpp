60c2446864772fc4ac031d8424827bf44c5ddfb2
Renggeng Zheng <rzhengcontacts@gmail.com>
2023-08-03 20:31:37 +0000

    yet another test case, all work so far

--- latency.cpp
+++ latency.cpp
@@ -1,11 +1,11 @@
-#include "main.hpp"
+#include "latency.hpp"
 
 int main(int argc, char* argv[])
 {
     // Defines the src occupancy map as a string.
-    std::string src_occupancy = "{ [xs, ys] -> [d0, d1] : d0=xs and d1=ys and 0 <= xs < 8 and 0 <= ys < 8 }";
+    std::string src_occupancy = "{ [xs, ys] -> [d0, d1] : d0 = xs and d1 = ys and 0 <= xs < 8 and 0 <= ys < 8 }";
     // Defines the dst fill map as a string.
-    std::string dst_fill = "{ [xd, yd] -> [d0, d1] : d0=xd and 0 <= d1 < 8 and 0 <= xd < 8 and 0 <= yd < 8 }";
+    std::string dst_fill = "{ [xd, yd] -> [d0, d1] : 0 <= d0 < 8 and 0 <= d1 < 8 and (xd=0 or 3<=xd<=4 or xd=7) and (yd=0 or 3<=yd<=4 or yd=7) }";
     // Defines the manhattan metric between two 2D points as a string.
     std::string manhattan_metric = "{"
         "[[xd, yd] -> [xs, ys]] -> [(xd - xs) + (yd - ys)] : "
