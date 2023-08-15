ee45300a891385454503882edae88fb81debe6d3
Renggeng Zheng <rzhengcontacts@gmail.com>
2023-08-15 16:23:51 +0000

    another test case

--- latency.cpp
+++ latency.cpp
@@ -3,9 +3,9 @@
 int main(int argc, char* argv[])
 {
     // Defines the src occupancy map as a string.
-    std::string src_occupancy = "{ [xs, ys] -> [d0, d1] : d0 = xs and 0 <= d1 < 8 and 0 <= xs < 8 and 0 <= ys < 8 }";
+    std::string src_occupancy = "{ [xs, ys] -> [d0, d1] : d0=xs and d1=ys and 0 <= xs < 8 and 0 <= ys < 8 }";
     // Defines the dst fill map as a string.
-    std::string dst_fill ="{ [xd, yd] -> [d0, d1] : d0 = xd and d1 = yd and 0 <= xd < 8 and 0 <= yd < 8 }";
+    std::string dst_fill ="{ [xd, yd] -> [d0, d1] : d0=xd and 0 <= d1 < 8 and 0 <= xd < 8 and 0 <= yd < 8 }";
 
     // Defines the torus circumference.
     int torus_circumference = 8;
